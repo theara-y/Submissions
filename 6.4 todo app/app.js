906466a3c6a63ddf152ee6a74183ae7bb4bfa17e
= <theara.ya@outlook.com>
2023-09-03 21:35:53 -0400

    changed data structure to object and implement local storage

--- 6.4 todo app/app.js
+++ 6.4 todo app/app.js
@@ -1,12 +1,13 @@
-let tasks = ["task1", "task2", "task3", "task4"];
+tasks = loadSaveData();
 
 let addBtn = document.querySelector("form button");
 let addTaskInput = document.querySelector("form input");
 
 addBtn.addEventListener("click", function(event) {
     event.preventDefault();
-    tasks.push(addTaskInput.value);
+    tasks.push({task: addTaskInput.value, isDone: false});
     render();
+    saveData();
 });
 
 let taskListElement = document.querySelector("ul");
@@ -15,9 +16,14 @@ taskListElement.addEventListener("click", function(event) {
     if(event.target.tagName === 'BUTTON') {
         let index = Number(event.target.parentNode.getAttribute("data-id"));
         tasks = tasks.slice(0, index).concat(tasks.slice(index + 1));
-        render();        
+        render();  
+        saveData();      
     } else if(event.target.tagName == "LI") {
         event.target.classList.toggle("cross-out");
+        let index = Number(event.target.getAttribute("data-id"));
+        tasks[index].isDone = !tasks[index].isDone;
+        console.log(tasks);
+        saveData();
     }
 });
 
@@ -28,10 +34,14 @@ function render() {
     }
 }
 
-function renderListItem(text, index) {
+function renderListItem(task, index) {
     let li = document.createElement("li");
-    li.textContent = text;
+    li.textContent = task.task;
     li.setAttribute("data-id", index);
+
+    if(task.isDone) {
+        li.classList.add("cross-out");
+    }
 
     let btn = document.createElement("button");
     btn.textContent = 'Delete';
@@ -40,6 +50,35 @@ function renderListItem(text, index) {
     li.prepend(btn);
 
     return li;
+}
+
+function saveData() {
+    localStorage.setItem("data", JSON.stringify(tasks));
+}
+
+function loadSaveData() {
+    let jsonString = localStorage.getItem("data");
+
+    let fakeData = [
+        {
+            "task": "Style the App",
+            "isDone": false,
+        },
+        {
+            "task": "Change the data structure",
+            "isDone": false,
+        },
+        {
+            "task": "Eat something",
+            "isDone": false,
+        },
+        {
+            "task": "Study for at least an hour each day",
+            "isDone": true,
+        },
+    ];
+
+    return JSON.parse(jsonString) || fakeData;
 }
 
 render();
